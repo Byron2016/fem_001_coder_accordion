306194f6320cf37d05de2bf54f90effb8064fd04
Byron 2016 <bgva2005@yahoo.com>
2022-02-07 17:26:46 -0500

    feat: accordion animation two

--- script.js
+++ script.js
@@ -8,37 +8,45 @@ document.querySelectorAll(".accordion__question").forEach((item) => {
     // source: https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
     //handle click
     // console.log("click!");
+    if (!item.classList.contains("collapsing")) {
+      let accCollapse = item.nextElementSibling;
 
-    let accCollapse = item.nextElementSibling;
+      // Open accordiion item
+      if (!item.classList.contains("open")) {
+        console.log("toggle accordion button");
 
-    // Open accordiion item
-    if (!item.classList.contains("open")) {
-      console.log("toggle accordion button");
+        accCollapse.style.display = "block";
+        let accHeight = accCollapse.clientHeight;
+        console.log(accHeight);
 
-      accCollapse.style.display = "block";
-      let accHeight = accCollapse.clientHeight;
-      console.log(accHeight);
-      accCollapse.style.height = accHeight + "px";
-      accCollapse.style.display = "";
+        setTimeout(() => {
+          accCollapse.style.height = accHeight + "px";
+          accCollapse.style.display = "";
+        }, 1);
 
-      accCollapse.classList = "accordion__collapse collapsing aa";
+        accCollapse.classList = "accordion__collapse collapsing aa";
 
-      setTimeout(() => {
-        console.log("open accordion content");
-        accCollapse.classList = "accordion__collapse open tt";
-        accCollapse.style.height = "";
-      }, 300);
-    }
-    // close accordiion item
-    else {
-      console.log("close");
+        setTimeout(() => {
+          console.log("open accordion content");
+          accCollapse.classList = "accordion__collapse collapse open tt";
+        }, 300);
+      }
+      // close accordiion item
+      else {
+        console.log("close");
 
-      accCollapse.classList = "accordion__collapse collapsing pp";
+        accCollapse.classList = "accordion__collapse collapsing pp";
 
-      setTimeout(() => {
-        console.log("close open accordion content ");
-        accCollapse.classList = "accordion__collapse collapse xxx";
-      }, 300);
+        setTimeout(() => {
+          accCollapse.style.height = "0px";
+        }, 1);
+
+        setTimeout(() => {
+          console.log("close open accordion content ");
+          accCollapse.classList = "accordion__collapse collapse xxx";
+          accCollapse.style.height = "";
+        }, 300);
+      }
     }
 
     item.classList.toggle("open");
