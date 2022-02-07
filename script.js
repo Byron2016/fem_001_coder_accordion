ed9aeea13d61c3c2ea079db0db8329406dd71aab
Byron 2016 <bgva2005@yahoo.com>
2022-02-07 16:40:23 -0500

    feat: accordion animation update js

--- script.js
+++ script.js
@@ -9,12 +9,39 @@ document.querySelectorAll(".accordion__question").forEach((item) => {
     //handle click
     // console.log("click!");
 
+    let accCollapse = item.nextElementSibling;
+
+    // Open accordiion item
+    if (!item.classList.contains("open")) {
+      console.log("toggle accordion button");
+
+      accCollapse.style.display = "block";
+      let accHeight = accCollapse.clientHeight;
+      console.log(accHeight);
+      accCollapse.style.height = accHeight + "px";
+      accCollapse.style.display = "";
+
+      accCollapse.classList = "accordion__collapse collapsing aa";
+
+      setTimeout(() => {
+        console.log("open accordion content");
+        accCollapse.classList = "accordion__collapse open tt";
+        accCollapse.style.height = "";
+      }, 300);
+    }
+    // close accordiion item
+    else {
+      console.log("close");
+
+      accCollapse.classList = "accordion__collapse collapsing pp";
+
+      setTimeout(() => {
+        console.log("close open accordion content ");
+        accCollapse.classList = "accordion__collapse collapse xxx";
+      }, 300);
+    }
+
     item.classList.toggle("open");
-
-    // Add "collapsing" class to .accordion__collapse (sibbling)
-
-    // Remove "collapsing" class and add "open" class
-
-    item.nextElementSibling.classList.toggle("open");
+    //item.nextElementSibling.classList.toggle("open");
   });
 });
