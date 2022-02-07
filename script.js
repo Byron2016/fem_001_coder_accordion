e92cbb24fafb883cb2a3cdf46e906a21c860a18d
Byron 2016 <bgva2005@yahoo.com>
2022-02-07 14:40:21 -0500

    feat: part_2 accordion question accessibility box

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 console.log("Hola");
+
+// document.querySelectorAll('.accordion__question')[0].classList.add('open');
+// document.querySelectorAll('.accordion__question')[0].classList.toggle('open');
