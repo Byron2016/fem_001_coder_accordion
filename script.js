9aa0309fee0e29147e44b7300108fb6b8f84a245
Byron 2016 <bgva2005@yahoo.com>
2022-02-07 15:05:48 -0500

    feat: accordion animation

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-console.log("Hola");
+// console.log("Hola");
 
 // document.querySelectorAll('.accordion__question')[0].classList.add('open');
 // document.querySelectorAll('.accordion__question')[0].classList.toggle('open');
+
+document.querySelectorAll(".accordion__question").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    // source: https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
+    //handle click
+    // console.log("click!");
+
+    item.classList.toggle("open");
+
+    // Add "collapsing" class to .accordion__collapse (sibbling)
+
+    // Remove "collapsing" class and add "open" class
+
+    item.nextElementSibling.classList.toggle("open");
+  });
+});
